5fba69aa9f1a94724f7f3ec62cba5e5bf29e174b
leonardorhein <leonardorheinheart@gmail.com>
2021-06-08 19:38:09 +0700

    Coba Stock

--- PCSUAS/Form1.Designer.cs
+++ PCSUAS/Form1.Designer.cs
@@ -29,21 +29,301 @@ namespace PCSUAS
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dbProjectUasDataSet = new PCSUAS.dbProjectUasDataSet();
+            this.m_barangBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.m_barangTableAdapter = new PCSUAS.dbProjectUasDataSetTableAdapters.m_barangTableAdapter();
+            this.tableAdapterManager = new PCSUAS.dbProjectUasDataSetTableAdapters.TableAdapterManager();
+            this.m_barangBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.m_barangBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.stockBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.stockTableAdapter = new PCSUAS.dbProjectUasDataSetTableAdapters.stockTableAdapter();
+            this.stockDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dbProjectUasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_barangBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_barangBindingNavigator)).BeginInit();
+            this.m_barangBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dbProjectUasDataSet
+            // 
+            this.dbProjectUasDataSet.DataSetName = "dbProjectUasDataSet";
+            this.dbProjectUasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // m_barangBindingSource
+            // 
+            this.m_barangBindingSource.DataMember = "m_barang";
+            this.m_barangBindingSource.DataSource = this.dbProjectUasDataSet;
+            // 
+            // m_barangTableAdapter
+            // 
+            this.m_barangTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.m_barangTableAdapter = this.m_barangTableAdapter;
+            this.tableAdapterManager.m_groupuserTableAdapter = null;
+            this.tableAdapterManager.m_hakaksesgroupuserTableAdapter = null;
+            this.tableAdapterManager.m_kotaTableAdapter = null;
+            this.tableAdapterManager.m_merkTableAdapter = null;
+            this.tableAdapterManager.m_modelTableAdapter = null;
+            this.tableAdapterManager.m_pelangganTableAdapter = null;
+            this.tableAdapterManager.m_supplierTableAdapter = null;
+            this.tableAdapterManager.m_usersTableAdapter = null;
+            this.tableAdapterManager.rework_detailTableAdapter = null;
+            this.tableAdapterManager.reworkTableAdapter = null;
+            this.tableAdapterManager.stock_historyTableAdapter = null;
+            this.tableAdapterManager.stockTableAdapter = this.stockTableAdapter;
+            this.tableAdapterManager.t_invoice_detailTableAdapter = null;
+            this.tableAdapterManager.t_invoice_headerTableAdapter = null;
+            this.tableAdapterManager.t_invoiceppn_detailTableAdapter = null;
+            this.tableAdapterManager.t_invoiceppn_headerTableAdapter = null;
+            this.tableAdapterManager.t_label_hargaTableAdapter = null;
+            this.tableAdapterManager.t_pembelian_detailTableAdapter = null;
+            this.tableAdapterManager.t_pembelian_headerTableAdapter = null;
+            this.tableAdapterManager.t_penawaran_detailTableAdapter = null;
+            this.tableAdapterManager.t_penawaran_headerTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = PCSUAS.dbProjectUasDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // m_barangBindingNavigator
+            // 
+            this.m_barangBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.m_barangBindingNavigator.BindingSource = this.m_barangBindingSource;
+            this.m_barangBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.m_barangBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.m_barangBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.m_barangBindingNavigatorSaveItem});
+            this.m_barangBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.m_barangBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.m_barangBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.m_barangBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.m_barangBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.m_barangBindingNavigator.Name = "m_barangBindingNavigator";
+            this.m_barangBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.m_barangBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.m_barangBindingNavigator.TabIndex = 0;
+            this.m_barangBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // m_barangBindingNavigatorSaveItem
+            // 
+            this.m_barangBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.m_barangBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("m_barangBindingNavigatorSaveItem.Image")));
+            this.m_barangBindingNavigatorSaveItem.Name = "m_barangBindingNavigatorSaveItem";
+            this.m_barangBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.m_barangBindingNavigatorSaveItem.Text = "Save Data";
+            this.m_barangBindingNavigatorSaveItem.Click += new System.EventHandler(this.m_barangBindingNavigatorSaveItem_Click);
+            // 
+            // stockBindingSource
+            // 
+            this.stockBindingSource.DataMember = "stock";
+            this.stockBindingSource.DataSource = this.dbProjectUasDataSet;
+            // 
+            // stockTableAdapter
+            // 
+            this.stockTableAdapter.ClearBeforeFill = true;
+            // 
+            // stockDataGridView
+            // 
+            this.stockDataGridView.AutoGenerateColumns = false;
+            this.stockDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.stockDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.stockDataGridView.DataSource = this.stockBindingSource;
+            this.stockDataGridView.Location = new System.Drawing.Point(0, 28);
+            this.stockDataGridView.Name = "stockDataGridView";
+            this.stockDataGridView.Size = new System.Drawing.Size(788, 220);
+            this.stockDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ID_STOCK";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ID_STOCK";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ID_BARANG";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID_BARANG";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "STOCK_MIN";
+            this.dataGridViewTextBoxColumn3.HeaderText = "STOCK_MIN";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "STOCK_ON_HAND";
+            this.dataGridViewTextBoxColumn4.HeaderText = "STOCK_ON_HAND";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stockDataGridView);
+            this.Controls.Add(this.m_barangBindingNavigator);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dbProjectUasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_barangBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_barangBindingNavigator)).EndInit();
+            this.m_barangBindingNavigator.ResumeLayout(false);
+            this.m_barangBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private dbProjectUasDataSet dbProjectUasDataSet;
+        private System.Windows.Forms.BindingSource m_barangBindingSource;
+        private dbProjectUasDataSetTableAdapters.m_barangTableAdapter m_barangTableAdapter;
+        private dbProjectUasDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator m_barangBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton m_barangBindingNavigatorSaveItem;
+        private dbProjectUasDataSetTableAdapters.stockTableAdapter stockTableAdapter;
+        private System.Windows.Forms.BindingSource stockBindingSource;
+        private System.Windows.Forms.DataGridView stockDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
     }
 }
 
--- PCSUAS/Form1.cs
+++ PCSUAS/Form1.cs
@@ -29,6 +29,10 @@ namespace PCSUAS
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'dbProjectUasDataSet.stock' table. You can move, or remove it, as needed.
+            this.stockTableAdapter.Fill(this.dbProjectUasDataSet.stock);
+            // TODO: This line of code loads data into the 'dbProjectUasDataSet.m_barang' table. You can move, or remove it, as needed.
+            this.m_barangTableAdapter.Fill(this.dbProjectUasDataSet.m_barang);
 
         }
 
@@ -37,5 +41,12 @@ namespace PCSUAS
 
         }
 
+        private void m_barangBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.m_barangBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dbProjectUasDataSet);
+
+        }
     }
 }

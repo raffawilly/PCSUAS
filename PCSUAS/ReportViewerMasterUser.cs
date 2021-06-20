1342c2dad28269294eed1e6bbd6421e20386df7c
Ferdinandus-Nicholas <nichocoolman@gmail.com>
2021-06-20 14:19:14 +0700

    Update Crystal Report

---
+++ PCSUAS/ReportViewerMasterUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCSUAS
+{
+    public partial class ReportViewerMasterUser : Form
+    {
+        public ReportViewerMasterUser()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            CrystalReportMasterUser report = new CrystalReportMasterUser();
+            crystalReportViewer1.ReportSource = report;
+        }
+    }
+}
